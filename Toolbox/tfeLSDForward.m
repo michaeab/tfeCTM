282d290b5283f34acac859329dae67554c5eeb80
Michael Barnett <micalan@sas.upenn.edu>
2022-04-20 12:21:43 -0400

    removed unused model

--- Toolbox/tfeLSDForward.m
+++ Toolbox/tfeLSDForward.m
@@ -1,4 +1,4 @@
-function [pcFromParams] = tfeCTMForward(params,stimuli)
+function [lagsFromParams] = tfeCTMRotMForward(params,stimuli,varargin)
 % Compute CTM forward model
 %
 % Synopsis
@@ -25,34 +25,43 @@ function [pcFromParams] = tfeCTMForward(params,stimuli)
 %     error('Bad values in CTM parameters structure');
 % end
 
+p = inputParser; p.KeepUnmatched = true;
+p.addRequired('params',@isvector);
+p.addRequired('stimuli',@ismatrix);
+p.addParameter('dimension',2,@isnumeric);
+p.addParameter('numMechanisms',2,@isnumeric);
+
+p.parse(params,stimuli,varargin{:});
+params = p.Results.params;
+
 %% Get the ellipsoid parameters in cannonical form
-dimension = size(stimuli,1);
+dimension   = p.Results.dimension;
+nMechanisms = p.Results.numMechanisms;
 if (dimension == 2)
-    % Loop over the number of mechaisms
+    %The one mechanism model
+    if nMechanisms == 1
+        R = deg2rotm(params(1));
+        E = [1 0; 0 params(2)];
+        m_hats = (R*E)'*stimuli;
+    elseif  nMechanisms == 2
+        R = deg2rotm(params(1));
+        E = [1 0; 0 1/params(2)];
+        m_hats = (R*E)'*stimuli;
+    else
+        error('Must be either 1 or 2 mechanisms');
+    end
     
-    for ii = 1:(size(params,1)-2)/2
-        
-        % Get the weight linear mechanism output
-        m_hat = abs(params((2*ii)-1).*stimuli(1,:) - params(2*ii).*stimuli(2,:));
-        
-        % Convert mechanism output to lags
-        pc_hat(ii,:) =  1-exp((m_hat./params(end-1)).^params(end));
-    end
+    % take the max of the mechanism outputs
+    m = vecnorm(m_hats);
 elseif (dimension == 3)
-    % Loop over the number of mechaisms
-    for ii = (size(params,1)-2)/2
-        % Get the weight linear mechanism output
-        m_hat = abs(params((3*ii)-2).*stimuli(1,:) + params((3*ii)-1).*stimuli(2,:) + params(3*ii).*stimuli(3,:));
-        
-        % Convert mechanism output to lags
-        pc_hat(ii,:) =  1-exp((m_hat./params(end-1)).^params(end));
-    end
+    error('3 Dimension case to come');
 else
     error('Dimension must be 2 or 3');
 end
 
-%% Take the min of the lags
-pcFromParams = max(pc_hat,[],1);
+% Convert mechanism output to lags
+lagsFromParams =  params(end-1) +  params(end).* exp(-1*m.*params(3));
+
 
 
 
